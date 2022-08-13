300993b77ed333ee02b609a7ffcae65e0db1bc8f
chengdashia <1847085602@qq.com>
2022-08-13 21:05:02 +0800

    Initial commit

---
+++ nyc-server/src/main/java/com/git/bds/nyc/NycServerApplication.java
@@ -0,0 +1,13 @@
+package com.git.bds.nyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NycServerApplication.class, args);
+    }
+
+}
---
+++ nyc-server/src/test/java/com/git/bds/nyc/NycServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.bds.nyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
