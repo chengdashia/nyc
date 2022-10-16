0f20bec6dfa04d66ec5664c39ce7c308bf201a20
chengdashia <1847085602@qq.com>
2022-10-16 13:59:13 +0800

    ...

---
+++ nyc-applet/nyc-applet-corp/src/main/java/com/git/bds/nyc/corp/service/impl/AuditCorpProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.git.bds.nyc.corp.service.impl;
+
+import com.git.bds.nyc.corp.dao.AuditCorpProductDao;
+import com.git.bds.nyc.corp.model.domain.AuditCorpProduct;
+import com.git.bds.nyc.corp.service.AuditCorpProductService;
+import com.github.yulichang.base.MPJBaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 审核公司发布的农产品(包括初级和加工的商品) 服务实现类
+ * </p>
+ *
+ * @author chnnc
+ * @since 2022-10-15 18:50:50
+ */
+@Service
+public class AuditCorpProductServiceImpl extends MPJBaseServiceImpl<AuditCorpProductDao, AuditCorpProduct> implements AuditCorpProductService {
+
+}
