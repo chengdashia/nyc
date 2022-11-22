7b251d8536359b23dfb36fbcc59948a16bfabd57
chengdashia <1847085602@qq.com>
2022-11-22 08:17:31 +0800

    收货地址

--- nyc-server/src/main/java/com/git/bds/nyc/NycServerApplication.java
+++ nyc-server/src/main/java/com/git/bds/nyc/NycServerApplication.java
@@ -3,7 +3,9 @@ package com.git.bds.nyc;
 import cn.easyes.starter.register.EsMapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Indexed;
 
+@Indexed
 @EsMapperScan("com.git.bds.nyc.**.mapper.ee")
 @SpringBootApplication
 public class NycServerApplication {
